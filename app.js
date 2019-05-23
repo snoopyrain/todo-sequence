e957186662103287ba1fa0a4834685f3b35e1cc7
林玉堂 <>
2019-05-22 22:48:39 -0700

    Initialize package

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const express = require('express')  //載入express
 const app = express()  //啟用express
 
+// 引用 express-handlebars
+const exphbs = require('express-handlebars');
+
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+const port = 3000
+
+//告訴express 使用 handlebars 當作template engine並預設layout 是main
+
+app.engine('handlebars',exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+
+app.use(bodyParser.urlencoded({extend: true}))
+app.use(methodOverride('_method'))
+
 //設定第一個首頁路由
 app.get('/', (req, res)=>{
-  res.send('hello world')
+  return res.render('index')
 })
 
 
 //設定express port 3000
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
   console.log('App is running')
 })
